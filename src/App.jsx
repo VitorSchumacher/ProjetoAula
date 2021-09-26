f9dff0009d497f252b32707fd7118cd7985820ad
Vitor E. Schumacher <dudu.schuminha@gmail.com>
2021-09-26 01:12:34 -0300

    Começando projetin

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Dias from './Comandos/Corpo';
+
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Vitor Schumacher</h1>
+      </header>
+      <body>
+        <Dias/>
+      </body>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Comandos/Corpo.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import "./Corpo.css";
+const Dias = () => {
+    return ( 
+        <>
+            <div>
+                <ul className="dias">
+                    <li className="dias-nome">Segunda</li>
+                    <li className="dias-nome">Terça</li>
+                    <li className="dias-nome">Quarta</li>
+                    <li className="dias-nome">Quinta</li>
+                    <li className="dias-nome">Sexta</li>
+                </ul>
+            </div>
+        </>
+     );
+}
+ 
+export default Dias;
